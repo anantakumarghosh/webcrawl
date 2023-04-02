22afe8798ecb35490d7662376f00cf9489ce2aae
ananta <antaghosh@gmail.com>
2023-04-02 14:39:34 +0530

    normalizeUrls

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,31 @@
 const {normalizeURL} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
-test('normalizeURL', () => {
-    const input = ''
+
+test('normalizeURL strip protocol', () => {
+    const input = 'https://www.1mg.com/path'
     const actual = normalizeURL(input)
-    const expected = ''
-    expect(actual).toEqual
+    const expected = 'www.1mg.com/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://www.1mg.com/path/'
+    const actual = normalizeURL(input)
+    const expected = 'www.1mg.com/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://WWW.1mg.com/path'
+    const actual = normalizeURL(input)
+    const expected = 'www.1mg.com/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'http://www.1mg.com/path'
+    const actual = normalizeURL(input)
+    const expected = 'www.1mg.com/path'
+    expect(actual).toEqual(expected)
 })
